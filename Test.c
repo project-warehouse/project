ec6604b2d4c8c074d10a71e65d378a4f34f6c6d0
Ramadhan Arif H <dani.ramadhan7@gmail.com>
2019-01-29 22:09:10 +0700

    Add files via upload

---
+++ Test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int a, b;
+  float salary = 1;
+  char letter = 'A';
+  a = 1;
+  b = 1;
+  int c = a+b;
+
+  printf("%d \n", c);
+  printf("%f \n", salary);
+  printf("%c \n", letter);
+
+  return 0;
+}
